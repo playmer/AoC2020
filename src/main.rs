e7e8e02037de7b1feca91f9dc588da1b769bfb2b
Joshua T. Fisher <playmer@gmail.com>
2020-12-04 22:41:08 -0800

    Working on part 4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::BufRead;
+use std::io::{BufRead};
 use std::str::FromStr;
+use std::collections::HashMap;
 
 
 fn lines_from_file(filename: impl AsRef<std::path::Path>) -> Vec<String> {
@@ -150,7 +151,7 @@ fn map_from_lines(lines : &Vec<String>) -> Map
     Map{tiles : lines.iter().map(line_to_tile_vec).collect::<Vec<Vec<TileType>>>()}
 }
 
-fn traverseMap(map : &Map, increase_x : usize, increase_y : usize) -> usize
+fn traverse_map(map : &Map, increase_x : usize, increase_y : usize) -> usize
 {
     let mut trees_hit : usize = 0;
     let mut position : (usize, usize) = (0, 0);
@@ -178,19 +179,182 @@ fn day_03()
 
     //map.tiles.len()
 
-    println!("    Part 1: {}", traverseMap(&map, 3, 1));
+    println!("    Part 1: {}", traverse_map(&map, 3, 1));
 
-    println!("    Part 2: {}",
-        traverseMap(&map, 1, 1)
-        * traverseMap(&map, 3, 1)
-        * traverseMap(&map, 5, 1)
-        * traverseMap(&map, 7, 1)
-        * traverseMap(&map, 1, 2));
+    println!("    Part 2: {}", traverse_map(&map, 1, 1)
+        * traverse_map(&map, 3, 1)
+        * traverse_map(&map, 5, 1)
+        * traverse_map(&map, 7, 1)
+        * traverse_map(&map, 1, 2));
 }
 
+
+struct Passport
+{
+    byr : String, //(Birth Year)
+    iyr : String, //(Issue Year)
+    eyr : String, //(Expiration Year)
+    hgt : String, //(Height)
+    hcl : String, //(Hair Color)
+    ecl : String, //(Eye Color)
+    pid : String, //(Passport ID)
+    cid : String, //(Country ID)
+}
+
+struct NorthPoleCredentials
+{
+    byr : String, //(Birth Year)
+    iyr : String, //(Issue Year)
+    eyr : String, //(Expiration Year)
+    hgt : String, //(Height)
+    hcl : String, //(Hair Color)
+    ecl : String, //(Eye Color)
+    pid : String, //(Passport ID)
+}
+
+enum PurportedPassport
+{
+    Passport(Passport),
+    NorthPoleCredentials(NorthPoleCredentials),
+    Other(HashMap<String,String>)
+}
+
+fn purported_passports_from_file(filename: impl AsRef<std::path::Path>) -> Vec<PurportedPassport> {
+    let file = std::fs::read_to_string(filename).expect("no such file");
+    let individual_passport_datas = file.split("\r\n\r\n").collect::<Vec<&str>>();
+
+    let mut purported_passports : Vec<PurportedPassport> = Vec::new();
+
+    for individual_passport_data in individual_passport_datas {
+        let mut purported_passport : HashMap<String, String> = HashMap::new();
+        let mut purported_passport_data = individual_passport_data.split(|c| c == ' ' || c == '\n' || c == '\r').collect::<Vec<&str>>();
+        purported_passport_data.retain(|&x| !x.is_empty());
+        for purported_passport_datum in purported_passport_data {
+            let item = purported_passport_datum.split(':').collect::<Vec<&str>>();
+            purported_passport.insert(String::from(item[0]), String::from(item[1]));
+        }
+
+        purported_passports.push(PurportedPassport::Other(purported_passport));
+    }
+
+    for purported_passport in purported_passports.iter_mut() {
+        match purported_passport
+        {
+            PurportedPassport::Other(possible) => {
+                if possible.contains_key("byr")
+                    && possible.contains_key("iyr")
+                    && possible.contains_key("eyr")
+                    && possible.contains_key("hgt")
+                    && possible.contains_key("hcl")
+                    && possible.contains_key("ecl")
+                    && possible.contains_key("pid")
+                {
+                    let byr = possible.get("byr").unwrap().to_string();
+                    let iyr = possible.get("iyr").unwrap().to_string();
+                    let eyr = possible.get("eyr").unwrap().to_string();
+                    let hgt = possible.get("hgt").unwrap().to_string();
+                    let hcl = possible.get("hcl").unwrap().to_string();
+                    let ecl = possible.get("ecl").unwrap().to_string();
+                    let pid = possible.get("pid").unwrap().to_string();
+                    if possible.contains_key("cid")
+                    {
+                        let cid = possible.get("cid").unwrap().to_string();
+                        *purported_passport = PurportedPassport::Passport(Passport{byr, iyr, eyr, hgt, hcl, ecl, pid, cid})
+                    }
+                    else
+                    {
+                        *purported_passport = PurportedPassport::NorthPoleCredentials(NorthPoleCredentials{byr, iyr, eyr, hgt, hcl, ecl, pid})
+                    }
+                }
+            },
+            _ => panic!()
+        }
+    }
+
+    purported_passports
+    //let others = individual_passport_datas.iter().map(|data| data.split(|c| c == ' ' || c == '\n')).collect::<Vec<Vec<&str>>>();
+
+    //.map(|pp| pp.split(|c| c == ' ' || c == '\n') ).collect::<Vec<Vec<String>>>()
+    //let buf = std::io::BufReader::new(file);
+}
+
+//fn data_to_purported_passport() -> PurportedPassport
+//{
+//    PurportedPassport
+//}
+
+fn day_04()
+{
+    println!("Day 4:");
+    let passports = purported_passports_from_file("adventInput_04.txt");
+
+    let mut quote_valids_unquote : usize = 0;
+    for passport in &passports {
+        match passport
+        {
+            PurportedPassport::Passport(_) | PurportedPassport::NorthPoleCredentials(_) => quote_valids_unquote += 1,
+            _ => {}
+        }
+    }
+
+    println!("    Part 1: {}", quote_valids_unquote);
+
+
+    for passport in &passports {
+        match passport
+        {
+            PurportedPassport::Passport(passport) => {
+                let mut valid = true;
+
+                //byr (Birth Year) - four digits; at least 1920 and at most 2002.
+                valid = valid && (passport.byr.len() == 4);
+                let birth_year = i32::from_str(passport.byr.as_str()).unwrap();
+                valid = valid && (birth_year >= 1920) && (birth_year <= 2002);
+
+                //iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+                valid = valid && (passport.iyr.len() == 4);
+                let issue_year = i32::from_str(passport.iyr.as_str()).unwrap();
+                valid = valid && (birth_year >= 2010) && (birth_year <= 2020);
+
+                //eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+                valid = valid && (passport.eyr.len() == 4);
+                let expiration_year = i32::from_str(passport.eyr.as_str()).unwrap();
+                valid = valid && (birth_year >= 2020) && (birth_year <= 2030);
+
+                //hgt (Height) - a number followed by either cm or in:
+                //If cm, the number must be at least 150 and at most 193.
+                //If in, the number must be at least 59 and at most 76.
+                //hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+                //ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+                valid = valid &&
+                    (passport.ecl == "amb"
+                    || passport.ecl == "blu"
+                    || passport.ecl == "brn"
+                    || passport.ecl == "gry"
+                    || passport.ecl == "grn"
+                    || passport.ecl == "hzl"
+                    || passport.ecl == "oth");
+                //pid (Passport ID) - a nine-digit number, including leading zeroes.
+                valid = valid && (passport.pid.len() == 9);
+                let expiration_year = i64::from_str(passport.pid.as_str()).unwrap();
+
+                if valid {
+                    quote_valids_unquote += 1
+                }
+            },
+            PurportedPassport::NorthPoleCredentials(_) => {
+                quote_valids_unquote += 1
+            },
+            _ => {}
+        }
+    }
+
+    println!("    Part 2: ");
+}
 
 fn main() {
     day_01();
     day_02();
     day_03();
+    day_04();
 }
